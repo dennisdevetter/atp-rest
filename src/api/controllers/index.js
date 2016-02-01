8c8b329c372655217ef8792d57f99e179fa37f63
dennisdevetter <dennis.de.vetter@telenet.be>
2016-02-01 14:50:09 +0100

    bla

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -2,7 +2,7 @@ import home from './home';
 import users from './users';
 import auth from './auth';
 
-export function createControllers() {
+export function createControllers(options) {
 	return [
 		home(options),
 		users(options),
--- test/unit/server.js
+++ test/unit/server.js
@@ -1,37 +1,13 @@
 var config = require('../../src/config').default;
 var apiModule = require('../../src/api').default;
 var dataStoreModule = require('../../src/database');
-var systemUnderTest = require('../../src/server');
 
 describe('The API REST server',() => {    
   it('should start', () => {                          
-  		
-  		var apiStub = root.sandbox.stub(apiModule, 'start');
-  		var dataStoreStub = root.sandbox.stub(dataStoreModule, 'configureDatabase').resolves('foo')().then((value) => {
-			//expect(dataStoreStub).to.have.been.calledWith(config.database);    
-
-			
-  		});
-
-  		//systemUnderTest.startServer();
+  		// TODO can this be tested if the module starts the server automatically ?
   });
 });  
 
 
 
 
-// // starts the rest api
-// var api = require('./api').default;
-// var config = require('./config').default;
-// var dataStore = require('./database');
-// var express = require('express');
-
-
-// dataStore.configureDatabase(config.database).then(() => {
-// 	console.log('database connection established, starting server...');
-// 	var app = express();
-// 	api.start({app, config});
-// }).catch((error) => {
-// 	console.log('Error connecting to the database. ' + error);
-// 	// todo.. send email ??
-// });
